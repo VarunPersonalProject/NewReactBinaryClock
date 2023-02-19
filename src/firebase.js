94288b2d43e0385079bfdfd508efa94cdb382459
Varun Gaikwad <gaikwadvarun23@gmail.com>
2023-02-19 22:54:40 +0530

    Done

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSZN_kfX0PbTymud3iTJllbtzYgGiy9v4",
+    authDomain: "home-page-91370.firebaseapp.com",
+    projectId: "home-page-91370",
+    storageBucket: "home-page-91370.appspot.com",
+    messagingSenderId: "771094136543",
+    appId: "1:771094136543:web:c19978cf9b17d3e965cb6a",
+  },
+  app = initializeApp(firebaseConfig);
+
+export default app;
